9297936a6e042375a4f7127d3028ea193ae0ff7f
toni <87004940+atortellini@users.noreply.github.com>
2026-02-12 03:35:21 -0800

    fixed constant name of NUM_TOTAL_BLOCKS to be NUM_TOTAL_BUCKETS as that is the number it actually represents.

--- path_oram/src/constants.h
+++ path_oram/src/constants.h
@@ -9,7 +9,7 @@ const uint32_t NUM_BYTES_PER_BLOCK = 8UL;
 
 const uint32_t NUM_TOTAL_REAL_BLOCKS = 1 << NUM_REAL_DATA_BLOCKS_AS_POWER_OF_2;
 const uint32_t HEIGHT_OF_TREE = NUM_REAL_DATA_BLOCKS_AS_POWER_OF_2;
-const uint32_t NUM_TOTAL_BLOCKS = (1 << (HEIGHT_OF_TREE + 1)) - 1;
+const uint32_t NUM_TOTAL_BUCKETS = (1 << (HEIGHT_OF_TREE + 1)) - 1;
 const uint32_t STASH_SIZE = 303UL + NUM_REAL_DATA_BLOCKS_AS_POWER_OF_2;
 // 303 was taken from Section 6.4 Fig. 5 as the required max size for a 256-bit
 // security parameter excluding the path initially retrieved from an access.
