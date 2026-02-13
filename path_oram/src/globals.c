50e8e66b673c3cf9aad63180c1a3cce6bbb6791c
toni <87004940+atortellini@users.noreply.github.com>
2026-02-13 14:24:06 -0800

    explicitly include stdint.h

--- path_oram/src/globals.c
+++ path_oram/src/globals.c
@@ -1,10 +1,12 @@
 #include "globals.h"
 #include "types.h"
 
+#include <stdint.h>
+
 Stash client_stash;
 uint32_t client_position_map[NUM_TOTAL_REAL_BLOCKS];
 Block DUMMY_BLOCK;
 
-EncryptedBucket server_storage[NUM_TOTAL_BUCKETS];  
+EncryptedBucket server_storage[NUM_TOTAL_BUCKETS];
 
 uint8_t synthetic_dataset[NUM_TOTAL_REAL_BLOCKS * NUM_BYTES_PER_BLOCK];
