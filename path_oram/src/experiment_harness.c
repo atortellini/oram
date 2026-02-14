15edca912437d02bac93da8f1c4c872813eceb25
toni <87004940+atortellini@users.noreply.github.com>
2026-02-14 00:43:04 -0800

    first implementation of an experiment harness. exposed function of perform_experiments that takes an argument for the query size that should be used. On invocation, populates the synthetic_dataset array with randomly generated bytes and invokes the client to access the Path ORAM and write the synthetic data onto the server. Populates the access sequence array to contain a random sequence of randomly selected range queries converted into point queries for sequential block ids. Then exercises the path oram through the sequene of accesses, only performing reads and not looking at any of the data returned for the moment. Could eventually add support for writes as seen with some of the commented out code. If I were to exercise writes I would not to determine whether it should be a fixed number of writes/reads, or randomly decided, and I would probably need to adjust the access sequence array to be an array of structs where the struct would contain the block id and the type of operation. Would also need anohter way to house the data to be written during the experiment so that the data doesn't have to be randomly generated on the fly.

---
+++ path_oram/src/experiment_harness.c
@@ -0,0 +1,163 @@
+#include "experiment_harness.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "client.h"
+#include "constants.h"
+#include "globals.h"
+#include "rand.h"
+#include "timer.h"
+
+#define min(X, Y) ((X) < (Y) ? (X) : (Y))
+
+// also need to think about for the experiments if I would want it to be a
+// random number of writes/reads being performed on random blocks or if there
+// should be parameters to specify how many reads/writes, what blocks to access,
+// etc.
+// Also question of whether I should generate all of the operations to be
+// performed prior to beginning the experiments as I expect the generation to
+// have impacts on the observed experiment times. Could have the entire access
+// order pre generated, along with predetermining whether a read/write should
+// occur and if a write, already generating the new data that should be written.
+
+void perform_experiments(const uint32_t query_size) {
+  prepare_synthetic_data();
+  generate_ranged_point_query_access_sequence(query_size);
+
+  struct timespec elapsed_time;
+  for (size_t i = 0; i < NUM_EXPERIMENT_ACCESSES; i++) {
+    TIME_VOID_FUNC(&elapsed_time,
+                   client_access(access_sequence_buffer[i], READ, NULL, NULL));
+    // DO SOMETHING WITH THIS TIMING INFORMATION LIKE WRITING TO A LOG FILE OR
+    // SOMETHING
+  }
+}
+
+static void prepare_synthetic_data(void) {
+  generate_synthetic_data();
+  insert_synthetic_data_into_server();
+}
+
+static void generate_synthetic_data(void) {
+  generate_random_bytes(synthetic_dataset,
+                        NUM_TOTAL_REAL_BLOCKS * NUM_BYTES_PER_BLOCK);
+}
+
+static void insert_synthetic_data_into_server(void) {
+  for (size_t i = 0; i < NUM_TOTAL_REAL_BLOCKS; i++) {
+    const uint32_t block_data_index = i * NUM_BYTES_PER_BLOCK;
+    client_access(i, WRITE, &synthetic_dataset[block_data_index], NULL);
+  }
+}
+
+static void
+generate_ranged_point_query_access_sequence(const uint32_t query_size) {
+  fill_access_sequence_with_ranged_point_queries(0, NUM_EXPERIMENT_ACCESSES,
+                                                 query_size);
+}
+
+static void
+fill_access_sequence_with_ranged_point_queries(uint32_t curr_index,
+                                               const uint32_t arr_size_left,
+                                               const uint32_t query_size) {
+  const bool noAccessesToGenerate = arr_size_left == 0;
+  const bool oneAccessToGenerate = arr_size_left == 1;
+  if (noAccessesToGenerate) {
+    return;
+  }
+
+  if (oneAccessToGenerate) {
+    access_sequence_buffer[curr_index] = uniform_random(NUM_TOTAL_REAL_BLOCKS);
+    return;
+  }
+
+  const uint32_t max_query_size = min(arr_size_left, query_size);
+  const uint32_t range_query_size = uniform_random(max_query_size) + 1;
+  const uint32_t end_index = curr_index + range_query_size;
+  const uint32_t start_block_range_id = uniform_random(NUM_TOTAL_REAL_BLOCKS);
+
+  for (size_t sequence_number = 0; curr_index < end_index;
+       sequence_number++, curr_index++) {
+    access_sequence_buffer[curr_index] =
+        (start_block_range_id + sequence_number) % NUM_TOTAL_REAL_BLOCKS;
+  }
+
+  const uint32_t new_arr_size_left = arr_size_left - range_query_size;
+  fill_access_sequence_with_ranged_point_queries(end_index, new_arr_size_left,
+                                                 query_size);
+}
+
+// I have an array of NUM_EXPERIMENT_ACCESSES size where, on each step, I want
+// to fill out a range of [1, QUERY_SIZE]. thinking of this recursively I have
+// something like this: fill_array(curr_index, arr_size_left, query_size) if
+// (arr_size_left == 0) return; const max_query_size = min(arr_size_left,
+// query_size) range_query_size = uniform_random(max_query_size) + 1; const
+// end_index = curr_index + range_query_size;
+//
+// for (curr_index < end_index; curr_index++) {
+//  access_sequence_buffer[curr_index] = uniform_random(NUM_TOTAL_REAL_BlOCKS) +
+//  1;
+//}
+// const new_arr_size_left = arr_size_left - range_query_size;
+// fill_array(curr_index, new_arr_size_left, query_size);
+
+// UNUSUED FUNCTIONS FOR EXPERIMENTS THAT MAY BE USED IF I DECIDE TO DO
+// EXPERIMENTS IN A DIFFERENT WAY e.g. I DECIDE TO ALLOW EXPERIMENTS TO PERFORM
+// WRITE OPERATIONS ON THE DATA. THIS CURRENTLY ISN'T IMPLEMENTED FOR A COUPLE
+// REASONS ONE I WOULD HAVE TO DEAL WITH HOW I WOULD ACQUIRE THE NEW DATA BYTES
+// TO BE WRITTEN, THIS CAN BE DONE ON THE FLY BUT THEN THAT WOULD MEAN EXTRA
+// OVERHEAD ON THE TIMING THAT ISN'T DIRECTLY RELATED TO HOW THE SCHEME
+// PERFORMS. TWO IF I WERE TO WANT TO MINIMIZE THE OVERHEAD (ONLY REQUIRING THE
+// COPYING OVER OF BYTES TO THE SYNETHIC DATASET ARRAY) IT WOULD REQUIRE ME TO
+// PREGENERATE ALL OF THE WRITE DATA THAT IS TO OCCUR DURING PARTICULAR
+// ACCESSES. THIS WOULD PROBABLY REQUIRE CHANGING THE ACCESS BUFFER TO BE OF
+// STRUCTS WHICH CONTAIN BOTH THE ID TO BE ACCSESED ALONG WITH WHETHER IT IS A
+// WRITE, THEN A SECOND ARRAY OF SYNTHETIC DATA LARGE ENOUGH TO COPE WITH ALL
+// THE WRITES THAT ARE TO BE PERFORMED.
+
+// void perform_experiments(const uint32_t num_accesses) {
+//   prepare_synthetic_data();
+
+//   for (size_t i = 0; i < num_accesses; i++) {
+//     const uint32_t block_to_access = uniform_random(NUM_TOTAL_REAL_BLOCKS);
+//     const bool performWrite = coin_flip();
+//     if (performWrite) {
+//       uint8_t new_block_data[NUM_BYTES_PER_BLOCK];
+//       generate_random_bytes(new_block_data, NUM_BYTES_PER_BLOCK);
+//       // probably want to change this so I can give the option of whether the
+//       // old data should be saved, for example if i want to have the option
+//       to
+//       // ensure correctness by comparing prev data
+//       client_access(block_to_access, WRITE, new_block_data, NULL);
+//       update_synthetic_block_data(block_to_access, new_block_data,
+//                                   NUM_BYTES_PER_BLOCK);
+
+//     } else {
+//       // again should probably have an option here to specify whether old
+//       data
+//       // should be kept if wanting to do some correctness checking
+//       client_access(block_to_access, READ, NULL, NULL);
+//     }
+//   }
+// }
+
+// static void update_synthetic_block_data(const uint32_t block_id,
+//                                         const uint8_t new_data[],
+//                                         const uint32_t num_bytes) {
+//   const bool tooManyBytes = num_bytes > NUM_BYTES_PER_BLOCK;
+//   const bool tooFewBytes = num_bytes < NUM_BYTES_PER_BLOCK;
+
+//   if (tooManyBytes || tooFewBytes) {
+//     fprintf(stderr, "Attempted to update synthetic block data with incorrect
+//     "
+//                     "number of bytes.");
+//     exit(1);
+//   }
+
+//   const uint32_t synthetic_data_index = block_id * NUM_BYTES_PER_BLOCK;
+
+//   mempcy(&synthetic_dataset[synthetic_data_index], num_bytes);
+// }
---
+++ path_oram/src/experiment_harness.h
@@ -0,0 +1,8 @@
+#ifndef EXPERIMENT_HARNESS_H
+#define EXPERIMENT_HARNESS_H
+
+#include <stdint.h>
+
+void perform_experiments(const uint32_t query_size);
+
+#endif
