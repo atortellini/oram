d96dbe69221f128c6e03e07e9d1fb8f7f47be952
toni <87004940+atortellini@users.noreply.github.com>
2026-02-13 16:25:54 -0800

    swapped place of macro argumetn func_call and func_output and formatted header.

--- path_oram/src/timer.h
+++ path_oram/src/timer.h
@@ -4,14 +4,14 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define TIME_FUNC(elapsed_time_ptr, func_output, func_call)                    \
+#define TIME_FUNC(elapsed_time_ptr, func_call, func_output)                    \
   do {                                                                         \
     struct timespec _start_time, _end_time;                                    \
     clock_gettime(CLOCK_MONOTONIC_RAW, &_start_time);                          \
     func_output = (func_call);                                                 \
     clock_gettime(CLOCK_MONOTONIC_RAW, &_end_time);                            \
-    (elapsed_time_ptr)->tv_sec = _end_time.tv_sec - _start_time.tv_sec;          \
-    (elapsed_time_ptr)->tv_nsec = _end_time.tv_nsec - _start_time.tv_nsec;       \
+    (elapsed_time_ptr)->tv_sec = _end_time.tv_sec - _start_time.tv_sec;        \
+    (elapsed_time_ptr)->tv_nsec = _end_time.tv_nsec - _start_time.tv_nsec;     \
     const bool _endedOnEarlierNanoSecond = (elapsed_time_ptr)->tv_nsec < 0;    \
     if (_endedOnEarlierNanoSecond) {                                           \
       (elapsed_time_ptr)->tv_sec--;                                            \
@@ -25,8 +25,8 @@
     clock_gettime(CLOCK_MONOTONIC_RAW, &_start_time);                          \
     (func_call);                                                               \
     clock_gettime(CLOCK_MONOTONIC_RAW, &_end_time);                            \
-    (elapsed_time_ptr)->tv_sec = _end_time.tv_sec - _start_time.tv_sec;          \
-    (elapsed_time_ptr)->tv_nsec = _end_time.tv_nsec - _start_time.tv_nsec;       \
+    (elapsed_time_ptr)->tv_sec = _end_time.tv_sec - _start_time.tv_sec;        \
+    (elapsed_time_ptr)->tv_nsec = _end_time.tv_nsec - _start_time.tv_nsec;     \
     const bool _endedOnEarlierNanoSecond = (elapsed_time_ptr)->tv_nsec < 0;    \
     if (_endedOnEarlierNanoSecond) {                                           \
       (elapsed_time_ptr)->tv_sec--;                                            \
