ccc3248e2beb0bac3cc601350fd12a614fdabc28
toni <87004940+atortellini@users.noreply.github.com>
2026-03-03 13:54:01 -0800

    Changed SUBORAM_read_range to take addresses_found_map as an argument rather than creating it for its own internal use. This is because the caller of the read_range function, RORAM_access, benefits from knowing which addresses were actually found for handling first time access to addresses where new blocks need to be created. Implemented second part of batch_evict that evicts blocks from the stash which intersect with any buckets in the paths, level by level. fill_bucket_with_blocks_from_... fills buckets out with dummy blocks if less than the bucket size is found in the stash. Something I'm not sure about which I also have in my path_oram code is iterating through the stash using stash->num_blocks while decrementing the amount through removing blocks. Could be an issue if the compiler optimizes to store the value of num blocks in a register rather than accessing the original memory each iteration to see if the value has been updated.
    Removed the bit_reverse_addition and swapping msb and lsb helper functions.

--- roram/src/suboram.c
+++ roram/src/suboram.c
@@ -24,15 +24,12 @@ void SUBORAM_init(SUBORAM *oram, const size_t suboram_index) {
 
 // assumes that results pointer will be of size range_size (2^i for subram R_i)
 uint32_t SUBORAM_read_range(SUBORAM *oram, const uint32_t address,
-                            BLOCK *results) {
+                            BLOCK *results, bool *addresses_found_map) {
+
   const uint32_t range_size = 1U << oram->oram_index;
   const uint32_t range_start_tag = oram->pm.map[address];
   const uint32_t new_range_tag = uniform_random(NUM_TOTAL_REAL_BLOCKS);
   oram->pm.map[address] = new_range_tag;
-
-  bool *addresses_found_map =
-      (bool *)calloc(range_size, sizeof(*addresses_found_map));
-  assert(addresses_found_map);
 
   find_and_record_blocks_found_in_range_from_stash(
       &oram->stash, results, addresses_found_map, address, range_size);
@@ -40,12 +37,12 @@ uint32_t SUBORAM_read_range(SUBORAM *oram, const uint32_t address,
   fetch_and_record_new_blocks_in_range_from_server(
       oram, results, addresses_found_map, address, range_start_tag, range_size);
 
-  free(addresses_found_map);
+  return new_range_tag;
 }
 
 void SUBORAM_batch_evict(SUBORAM *oram, const size_t num_evictions) {
 
-  for (int level = HEIGHT_OF_TREE; level >= 0; level--) {
+  for (size_t level = 0; level <= HEIGHT_OF_TREE; level++) {
     const uint32_t num_max_buckets = 1 << level;
     const uint32_t bounded_num_buckets = min(num_max_buckets, num_evictions);
     const uint32_t bounded_num_blocks =
@@ -58,9 +55,121 @@ void SUBORAM_batch_evict(SUBORAM *oram, const size_t num_evictions) {
     const size_t num_blocks_found = read_buckets_at_level_along_paths(
         oram, level, eviction_counter, num_evictions, blocks_found);
 
-    insert_new_blocks_into_stash(oram->stash, blocks_found, num_blocks_found);
+    insert_new_blocks_into_stash(&oram->stash, blocks_found, num_blocks_found);
 
     free(blocks_found);
+  }
+
+  // I think I'll just write back the buckets back to the server per level
+  // rather than collecting the entire set of buckets that need to be written
+  // across all levels then doing it all at once the reason is I can't really
+  // think of a structure to store the buckets in that would be easy to access
+  // afterwards when going to write all the buckets back, also to actually
+  // insert the buckets into the structure. Also a thing to note like my other
+  // functions I can limit the number of iterations over the stash per level to
+  // just be min(num_evictions, num_buckets_at_level). This is because each
+  // eviction path is sequential and is modulus the number of buckets in the
+  // level. Therefore, each subsequent eviction path will be unique and, if the
+  // number of evictions were to be greater than the number of buckets in the
+  // level, all unique buckets will have been exhausted.
+  for (int level = HEIGHT_OF_TREE; level >= 0; level--) {
+    const uint32_t num_buckets_at_level = 1 << level;
+    const uint32_t bounded_num_unique_paths =
+        min(num_buckets_at_level, num_evictions);
+
+    BUCKET *new_buckets_at_level = (BUCKET *)malloc(
+        sizeof(*new_buckets_at_level) * bounded_num_unique_paths);
+    assert(new_buckets_at_level);
+
+    for (uint32_t path_offset = 0; path_offset < bounded_num_unique_paths;
+         path_offset++) {
+      const uint32_t path = eviction_counter + path_offset;
+      BLOCK *bucket_buffer = new_buckets_at_level[path_offset].blocks;
+
+      fill_bucket_with_blocks_from_stash_intersecting_path_at_level(
+          oram, bucket_buffer, path, level);
+    }
+
+    write_bucket_range_to_server(oram, new_buckets_at_level, eviction_counter,
+                                 bounded_num_unique_paths, level);
+
+    free(new_buckets_at_level);
+  }
+}
+
+static void write_bucket_range_to_server(const SUBORAM *oram,
+                                         const BUCKET *buckets,
+                                         const uint32_t base_path,
+                                         const uint32_t num_buckets,
+                                         const uint32_t level) {
+  const uint32_t num_buckets_at_level = 1U << level;
+  const uint32_t num_buckets_above_level = (1U << level) - 1;
+
+  ENCRYPTED_BUCKET *encrypted_buckets =
+      (ENCRYPTED_BUCKET *)malloc(sizeof(*encrypted_buckets) * num_buckets);
+  assert(encrypted_buckets);
+
+  encrypt_buckets(buckets, encrypted_buckets, num_buckets);
+
+  for (uint32_t i = 0; i < num_buckets; i++) {
+    const uint32_t bit_reversed_bucket_id_at_level =
+        (base_path + i) % num_buckets_at_level;
+    const uint32_t target_bucket_id_in_suboram =
+        bit_reversed_bucket_id_at_level + num_buckets_above_level;
+    const uint32_t absolute_bucket_id =
+        oram->oram_index * NUM_BUCKETS_PER_ORAM + target_bucket_id_in_suboram;
+
+    SERVER_write_bucket(absolute_bucket_id, &encrypted_buckets[i]);
+  }
+
+  free(encrypted_buckets);
+}
+
+static void encrypt_buckets(const BUCKET *plaintext_buckets,
+                            ENCRYPTED_BUCKET *encrypted_buckets,
+                            const uint32_t num_buckets) {
+  for (uint32_t i = 0; i < num_buckets; i++) {
+    for (uint32_t j = 0; j < NUM_BLOCKS_PER_BUCKET; j++) {
+      CRYPTO_encrypt_block(&plaintext_buckets[i].blocks[j],
+                           &encrypted_buckets[i].blocks[j]);
+    }
+  }
+}
+
+static void fill_bucket_with_blocks_from_stash_intersecting_path_at_level(
+    SUBORAM *oram, BLOCK *bucket_buffer, const uint32_t path_tag,
+    const uint32_t level) {
+  STASH *stash = &oram->stash;
+  const uint32_t num_buckets_at_level = 1U << level;
+  const uint32_t evicted_path_at_level = path_tag % num_buckets_at_level;
+
+  size_t found_block_counter = 0;
+
+  for (size_t i = 0; i < stash->num_blocks;) {
+    const BLOCK *candidate_block = &stash->stash[i];
+    const uint32_t candidate_path_at_level =
+        candidate_block->path_tags[oram->oram_index] % num_buckets_at_level;
+    const bool candidatePathIntersectsEvictedPath =
+        candidate_path_at_level == evicted_path_at_level;
+
+    if (candidatePathIntersectsEvictedPath) {
+      bucket_buffer[found_block_counter++] = *candidate_block;
+      STASH_remove_block(oram->stash, i);
+      const bool bucketBufferIsFull =
+          found_block_counter == NUM_BLOCKS_PER_BUCKET;
+      if (bucketBufferIsFull) {
+        break;
+      }
+    } else {
+      i++;
+    }
+  }
+
+  const bool bucketIsUnderfilled = found_block_counter < NUM_BLOCKS_PER_BUCKET;
+  if (bucketIsUnderfilled) {
+    for (size_t i = found_block_counter; i < NUM_BLOCKS_PER_BUCKET; i++) {
+      bucket_buffer[i] = DUMMY_BLOCK;
+    }
   }
 }
 
@@ -155,7 +264,7 @@ static size_t read_buckets_at_level_along_paths(const SUBORAM *oram,
 
   for (uint32_t i = 0; i < num_buckets_to_access; i++) {
     const uint32_t bit_reversed_bucket_id_at_level =
-        range_start_tag + i % num_buckets_at_level;
+        (range_start_tag + i) % num_buckets_at_level;
     const uint32_t target_bucket_id_in_suboram =
         bit_reversed_bucket_id_at_level + num_buckets_above_level;
     const uint32_t absolute_bucket_id =
@@ -214,29 +323,4 @@ static void filter_and_record_new_blocks(BLOCK *blocks, const size_t num_blocks,
       }
     }
   }
-}
-
-static uint32_t bit_reverse_addition(const uint32_t base, const uint32_t addend,
-                                     const uint32_t msb_index) {
-  if (msb_index == 0)
-    return 0;
-  if (addend == 0)
-    return base;
-
-  return swap_msb_and_lsb((swap_msb_and_lsb(base, msb_index) + addend),
-                          msb_index);
-}
-
-static uint32_t swap_msb_and_lsb(const uint32_t x, const uint32_t msb_index) {
-  const uint32_t msb_flag = 1U << msb_index;
-  const uint32_t lsb_flag = 1U;
-
-  const uint32_t bits_0_to_msb_set = (1 << (msb_index + 1)) - 1;
-  const uint32_t all_bits_but_msb_and_lsb_set = ~(msb_flag | lsb_flag);
-
-  const uint32_t intermediate_bits_flag =
-      all_bits_but_msb_and_lsb_set & bits_0_to_msb_set;
-
-  return ((x & msb_flag) >> msb_index) | ((x & lsb_flag) << msb_index) |
-         (x & intermediate_bits_flag);
 }
--- roram/src/suboram.h
+++ roram/src/suboram.h
@@ -1,6 +1,9 @@
 #ifndef SUBORAM_H
 #define SUBORAM_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include "types.h"
 
 typedef struct suboram_t {
@@ -8,5 +11,12 @@ typedef struct suboram_t {
   POSITION_MAP pm;
   uint32_t oram_index;
 } SUBORAM;
+
+void SUBORAM_init(SUBORAM *oram, const size_t suboram_index);
+
+uint32_t SUBORAM_read_range(SUBORAM *oram, const uint32_t address,
+                            BLOCK *results, bool *addresses_found_map);
+
+void SUBORAM_batch_evict(SUBORAM *oram, const size_t num_evictions);
 
 #endif
