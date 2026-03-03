2322083ab5e44cec0cfb3496aa23967edb583348
toni <87004940+atortellini@users.noreply.github.com>
2026-03-03 15:58:55 -0800

    defined globals for eviction counter and dummy block.

--- roram/src/globals.h
+++ roram/src/globals.h
@@ -1,7 +1,15 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <stdint.h>
 
+#include "roram.h"
+#include "types.h"
 
+extern BLOCK DUMMY_BLOCK;
+
+extern RORAM rORAM;
+extern uint32_t EVICTION_COUNTER;
+extern ENCRYPTED_BUCKET server_storage[NUM_TOTAL_BUCKETS];
 
 #endif
