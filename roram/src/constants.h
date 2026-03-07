af78c0b13af260afa99e78930b4e72d1e04baa48
toni <87004940+atortellini@users.noreply.github.com>
2026-03-07 00:07:09 -0800

    updated stash size to some number, think it's much larger than it needs to be but that's alright

--- roram/src/constants.h
+++ roram/src/constants.h
@@ -9,7 +9,9 @@
 #define HEIGHT_OF_TREE NUM_REAL_DATA_BLOCKS_AS_POWER_OF_2
 #define NUM_BUCKETS_PER_ORAM ((1U << (HEIGHT_OF_TREE + 1U)) - 1)
 #define NUM_TOTAL_BUCKETS (NUM_BUCKETS_PER_ORAM * NUM_SUBORAMS)
-#define STASH_SIZE 5 // FIXME: FIX THE STASH SIZE IDK WHAT IT SHOULD BE
+#define STASH_SIZE                                                             \
+  ((303U + HEIGHT_OF_TREE) *                                                   \
+   QUERY_RANGE_SIZE) // FIXME: FIX THE STASH SIZE IDK WHAT IT SHOULD BE
 
 #define AES_KEY_SIZE_BYTES 32U
 #define AES_IV_SIZE_BYTES 12U
