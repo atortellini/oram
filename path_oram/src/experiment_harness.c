fbde531e0d7570ad96eca0a11cc02475e1c9ae25
toni <87004940+atortellini@users.noreply.github.com>
2026-03-17 15:35:46 -0700

    improved both experiment harnesses to perform logr + 1 sequences of NUM_EXPERIMENT_ACCESSES range queries where r is the maximum range size. It performs range size queries starting from 1 and doubling until r. This is better since I only have to incur the very expensive cost of inserting all of the synthetic data once to perform all of the experiments I need.

--- path_oram/src/experiment_harness.c
+++ path_oram/src/experiment_harness.c
@@ -1,10 +1,10 @@
 #include "experiment_harness.h"
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #include "../../parameters/parameters.h"
 #include "client.h"
@@ -19,32 +19,49 @@ typedef struct {
   uint32_t queries[QUERY_RANGE_SIZE];
 } Access;
 
+typedef struct {
+  Access accesses[NUM_EXPERIMENT_ACCESSES];
+  uint32_t range_size;
+} Access_Buffer;
+
 static uint8_t *synthetic_dataset;
-static Access access_sequence_buffer[NUM_EXPERIMENT_ACCESSES];
+static Access_Buffer access_sequence_buffer[QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1];
 
 static void prepare_synthetic_data(void);
 static void generate_synthetic_data(void);
 static void insert_synthetic_data_into_server(void);
 
-static void generate_ranged_point_query_access_sequence(void);
-static void perform_access(const uint32_t access_index);\
+static void initialize_access_sequence_buffer(void);
+static void generate_ranged_point_query_access_sequence(
+    Access *accesses, const size_t num_accesses, const uint32_t range_size);
+static void perform_access(const Access *access, const uint32_t range_size);
 
 void perform_experiments(void) {
   prepare_synthetic_data();
-  generate_ranged_point_query_access_sequence();
+
+  initialize_access_sequence_buffer();
 
   struct timespec elapsed_time;
 
-  for (size_t access_index = 0; access_index < NUM_EXPERIMENT_ACCESSES;
-       access_index++) {
-    TIME_VOID_FUNC(&elapsed_time, perform_access(access_index));
-    fprintf(stdout, "Access %lu took: %ld s %ld ns\n", access_index,
-            elapsed_time.tv_sec, elapsed_time.tv_nsec);
+  for (size_t access_buff = 0;
+       access_buff < (QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1); access_buff++) {
+    const Access_Buffer *curr_buff = &access_sequence_buffer[access_buff];
+    const uint32_t curr_range_size = curr_buff->range_size;
+
+    fprintf(stdout, "Performing accesses for range %u...\n", curr_range_size);
+
+    for (size_t i = 0; i < NUM_EXPERIMENT_ACCESSES; i++) {
+      TIME_VOID_FUNC(&elapsed_time,
+                     perform_access(&curr_buff->accesses[i], curr_range_size));
+      fprintf(stdout, "Access %lu took: %ld s %ld ns\n", i, elapsed_time.tv_sec,
+              elapsed_time.tv_nsec);
+    }
   }
 }
 
 static void prepare_synthetic_data(void) {
-  synthetic_dataset = (uint8_t *)malloc(sizeof(uint8_t) * NUM_TOTAL_REAL_BLOCKS * NUM_DATA_BYTES_PER_BLOCK);
+  synthetic_dataset = (uint8_t *)malloc(
+      sizeof(uint8_t) * NUM_TOTAL_REAL_BLOCKS * NUM_DATA_BYTES_PER_BLOCK);
   assert(synthetic_dataset);
 
   struct timespec e;
@@ -68,22 +85,37 @@ static void insert_synthetic_data_into_server(void) {
   }
 }
 
-static void generate_ranged_point_query_access_sequence(void) {
-  for (size_t access_index = 0; access_index < NUM_EXPERIMENT_ACCESSES;
-       access_index++) {
-    const uint32_t start_block_range_id = uniform_random(NUM_TOTAL_REAL_BLOCKS - (QUERY_RANGE_SIZE - 1));
-    for (size_t query_num = 0; query_num < QUERY_RANGE_SIZE; query_num++) {
-      access_sequence_buffer[access_index].queries[query_num] =
+static void initialize_access_sequence_buffer(void) {
+  for (size_t i = 0; i < (QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1); i++) {
+    Access_Buffer *curr_access_buffer = &access_sequence_buffer[i];
+    const uint32_t curr_range_size = 1U << i;
+
+    curr_access_buffer->range_size = curr_range_size;
+    generate_ranged_point_query_access_sequence(
+        curr_access_buffer->accesses, NUM_EXPERIMENT_ACCESSES, curr_range_size);
+  }
+}
+
+static void generate_ranged_point_query_access_sequence(
+    Access *accesses, const size_t num_accesses, const uint32_t range_size) {
+  const uint32_t last_valid_base_address = NUM_TOTAL_REAL_BLOCKS - range_size;
+  const uint32_t base_address_upper_bound = last_valid_base_address + 1;
+
+  for (size_t access_index = 0; access_index < num_accesses; access_index++) {
+    const uint32_t start_block_range_id =
+        uniform_random(base_address_upper_bound);
+
+    for (size_t query_num = 0; query_num < range_size; query_num++) {
+      accesses[access_index].queries[query_num] =
           (start_block_range_id + query_num) % NUM_TOTAL_REAL_BLOCKS;
     }
   }
 }
 
-static void perform_access(const uint32_t access_index) {
-  const Access *access = &access_sequence_buffer[access_index];
+static void perform_access(const Access *access, const uint32_t range_size) {
   uint8_t returned_data[NUM_DATA_BYTES_PER_BLOCK];
 
-  for (size_t query_index = 0; query_index < QUERY_RANGE_SIZE; query_index++) {
+  for (size_t query_index = 0; query_index < range_size; query_index++) {
     const uint32_t query_block_id = access->queries[query_index];
     const uint8_t *expected_data =
         &synthetic_dataset[query_block_id * NUM_DATA_BYTES_PER_BLOCK];
--- roram/src/experiment_harness.c
+++ roram/src/experiment_harness.c
@@ -19,27 +19,43 @@ typedef struct access_t {
   uint32_t base_address;
 } ACCESS;
 
+typedef struct access_buffer_t {
+  ACCESS accesses[NUM_EXPERIMENT_ACCESSES];
+  uint32_t range_size_po2;
+} ACCESS_BUFFER;
+
 static uint8_t *synthetic_dataset;
-static ACCESS access_sequence_buffer[NUM_EXPERIMENT_ACCESSES];
+static ACCESS_BUFFER access_sequence_buffer[QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1];
 
 static void prepare_synthetic_data(void);
 static void generate_synthetic_data(void);
 static void insert_synthetic_data_into_server(void);
 
-static void generate_range_query_access_sequence(void);
-static void perform_access(const uint32_t access_index);
+static void initialize_access_sequence_buffer(void);
+static void generate_range_query_access_sequence(ACCESS *accesses,
+                                                 const size_t num_accesses,
+                                                 const uint32_t range_size);
+static void perform_access(const ACCESS *access, const uint32_t range_size_po2);
 
 void perform_experiments(void) {
   prepare_synthetic_data();
-  generate_range_query_access_sequence();
+
+  initialize_access_sequence_buffer();
 
   struct timespec elapsed_time;
+  for (size_t access_buff = 0;
+       access_buff < (QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1); access_buff++) {
+    const ACCESS_BUFFER *curr_buff = &access_sequence_buffer[access_buff];
+    const uint32_t curr_range_size = 1U << curr_buff->range_size_po2;
 
-  for (size_t access_index = 0; access_index < NUM_EXPERIMENT_ACCESSES;
-       access_index++) {
-    TIME_VOID_FUNC(&elapsed_time, perform_access(access_index));
-    fprintf(stdout, "Access %lu took: %ld s %ld ns\n", access_index,
-            elapsed_time.tv_sec, elapsed_time.tv_nsec);
+    fprintf(stdout, "Performing accesses for range %u...\n", curr_range_size);
+
+    for (size_t i = 0; i < NUM_EXPERIMENT_ACCESSES; i++) {
+      TIME_VOID_FUNC(&elapsed_time, perform_access(&curr_buff->accesses[i],
+                                                   curr_buff->range_size_po2));
+      fprintf(stdout, "Access %lu took: %ld s %ld ns\n", i, elapsed_time.tv_sec,
+              elapsed_time.tv_nsec);
+    }
   }
 }
 
@@ -69,30 +85,42 @@ static void insert_synthetic_data_into_server(void) {
   }
 }
 
-static void generate_range_query_access_sequence(void) {
-  const uint32_t last_valid_base_address =
-      NUM_TOTAL_REAL_BLOCKS - QUERY_RANGE_SIZE;
+static void initialize_access_sequence_buffer(void) {
+  for (size_t i = 0; i < (QUERY_RANGE_SIZE_AS_POWER_OF_2 + 1); i++) {
+    ACCESS_BUFFER *curr_access_buffer = &access_sequence_buffer[i];
+    const uint32_t curr_range_size = 1U << i;
+
+    curr_access_buffer->range_size_po2 = i;
+
+    generate_range_query_access_sequence(
+        curr_access_buffer->accesses, NUM_EXPERIMENT_ACCESSES, curr_range_size);
+  }
+}
+
+static void generate_range_query_access_sequence(ACCESS *accesses,
+                                                 const size_t num_accesses,
+                                                 const uint32_t range_size) {
+  const uint32_t last_valid_base_address = NUM_TOTAL_REAL_BLOCKS - range_size;
   const uint32_t base_address_upper_bound = last_valid_base_address + 1;
 
-  for (size_t access_index = 0; access_index < NUM_EXPERIMENT_ACCESSES;
-       access_index++) {
-    access_sequence_buffer[access_index].base_address =
+  for (size_t access_index = 0; access_index < num_accesses; access_index++) {
+    accesses[access_index].base_address =
         uniform_random(base_address_upper_bound);
   }
 }
 
-static void perform_access(const uint32_t access_index) {
-  const uint32_t query_base_address =
-      access_sequence_buffer[access_index].base_address;
-  uint8_t returned_data[NUM_DATA_BYTES_PER_BLOCK * QUERY_RANGE_SIZE];
+static void perform_access(const ACCESS *access,
+                           const uint32_t range_size_po2) {
+  const uint32_t query_base_address = access->base_address;
+  const uint32_t range_size = 1U << range_size_po2;
+  uint8_t returned_data[NUM_DATA_BYTES_PER_BLOCK * range_size];
 
   const uint8_t *expected_data =
       &synthetic_dataset[query_base_address * NUM_DATA_BYTES_PER_BLOCK];
-  CLIENT_access(query_base_address, QUERY_RANGE_SIZE_AS_POWER_OF_2, READ, NULL,
-                returned_data);
+  CLIENT_access(query_base_address, range_size_po2, READ, NULL, returned_data);
 
   if (memcmp(expected_data, returned_data,
-             NUM_DATA_BYTES_PER_BLOCK * QUERY_RANGE_SIZE) != 0) {
+             NUM_DATA_BYTES_PER_BLOCK * range_size) != 0) {
     fprintf(stderr,
             "Comparison of data for block range starting at: %u differs from "
             "expected.\n",
